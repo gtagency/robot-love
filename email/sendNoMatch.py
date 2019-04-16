58fefd67f987ad9d8e23810c7c986f74505162d2
Kyle <kwaldner3@gatech.edu>
2019-04-16 19:23:40 -0400

    scripts

--- email/sendNoMatch.py
+++ email/sendNoMatch.py
@@ -4,32 +4,32 @@ import csv
 #run in python 2, idk if python 3 works for this
 #need to follow instructions in the README
 
-baseSubject = 'Subject: Your Love Machine Match has been found'
+baseSubject = 'Subject: Love Machine Results'
 
-baseMessage = 'You matched with a person using this email address '
+baseMessage = "We are sorry to inform you that we were unable to find a match for you. This was due to a couple different factors including the ratios of students who filled out the survey and the nature of the responses we received. We apologize but we believe that you can find love on your own, so dont give up!"
 
 commandStringBase = 'cat mailMessage | msmtp '
 
-with open('addresses.csv') as csv_file:
+with open('NoMatch.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
     lineCount = 0
     for row in csv_reader:
         reciever = row[0]
         match = row[1]
         if match == 'Nobody':
-            print('did not send ' + reciever)
-        else:
-            print('sent mail to ' + reciever + ' they matched with ' + match)
+            print('sent mail to ' + reciever)
             message = open('mailMessage', 'r+')
             message.truncate(0)
             message.write(baseSubject)
             message.write('\n')
             message.write('\n')
-            personalMessage = baseMessage + match
+            personalMessage = baseMessage
             message.write(personalMessage)
             message.close()
             commandString = commandStringBase + reciever
             subprocess.call(commandString, shell = True)
             lineCount += 1
+        else:
+            print('did not send ' + reciever)
     lineCountStr = str(lineCount)
     print('mail sent to ' + lineCountStr + ' addresses')
